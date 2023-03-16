b6f367d9d4bfa353ca78dd92f23bad6d854b24d0
Rícharde de Oliveira Lira <richarde.lira@lead.fortaleza>
2023-03-16 20:15:58 -0300

    adding files

---
+++ Frontend/test.py
@@ -0,0 +1,7 @@
+# ydl1.py
+from __future__ import unicode_literals
+import youtube_dl
+
+ydl_opts = {}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=dP15zlyra3c'])
