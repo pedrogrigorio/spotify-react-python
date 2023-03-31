15434d796dc1db91dd58d096a26f044fe0ed0688
Rícharde de Oliveira Lira <richarde.lira@lead.fortaleza>
2023-03-31 15:46:09 -0300

    rebase branch

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,24 +1,53 @@
-import { useState } from 'react';
-import { Provider } from 'react-redux';
-import store from './components/Store/index'
-import Footer from './components/Footer/Footer';
-import SideBar from './components/SideBar/SideBar'
-import styles from './App.module.css';
+import './App.css';
+import Container from './components/layout/Container/Container';
+import Footer from './components/layout/Footer/Footer';
+import SideMenu from './components/layout/SideMenu/SideMenu';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Home from './components/pages/Home';
-import Search from './components/pages/Search';
-import Library from './components/pages/Library';
-import LikedSongs from './components/pages/LikedSongs';
-import Container from './components/pages/Container';
-
+import { useState } from 'react';
+import Home from './pages/Home/Home';
+import Search from './pages/Search/Search';
+import Library from './pages/Library/Library';
+import LikedSongs from './pages/LikedSongs/LikedSongs';
+import teste from './assets/img/teste.jpg'
 
 function App() {
 
+  const [songs, setSongs] = useState({})
+
+  async function searchMusic() {
+    
+    let url_music = 'http://127.0.0.1:8000/read_music'
+    let url_info  = 'http://127.0.0.1:8000/read_title'
+    fetch(url_music)
+            .then((res) => { return res.blob(); })
+            .then((data) => {
+            var a = document.createElement("a");
+            a.href = window.URL.createObjectURL(data);
+
+            fetch(url_info).then((response) => response.json())
+            .then((data) => { 
+
+            setSongs(
+              {
+               title   : data.title,
+               artist  : data.artist,
+               img_src : data.img,
+               src     : a
+              }
+          )
+
+        });  
+    });
+  }
+
+  function readInfo() { 
+    console.log(songs.img_src)
+  }
+
   return (
-    <div className={styles.layout}>
-        <Router>
-        <Provider store={store}>
-        <SideBar />
+    <div className="App">
+      <Router>
+        <SideMenu />
         <Container>
           <Routes>
             <Route path='/' element={<Home />}></Route>
@@ -26,9 +55,9 @@ function App() {
             <Route path='/library' element={<Library />}></Route>
             <Route path='/liked-songs' element={<LikedSongs />}></Route>
           </Routes>
+          <button onClick={() => searchMusic()}>Botão</button>
         </Container>
-        <Footer/>  
-        </Provider>
+        <Footer img={songs.img_src} title={songs.title} artist={songs.artist} trackData={songs.src}/>
       </Router>
     </div>
   );
---
+++ Frontend/src/components/ui/PlayerSlider/PlayerSlider.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Slider from '../Slider/Slider'
+import convertTime from '../../../helpers/convertTime'
+import styles from './PlayerSlider.module.css'
+
+export default function ProgressBar(props) {
+    return (
+            <div className={styles.progress_bar_container}>
+                <p className={styles.timer_start}>{convertTime(props.currentTime)}</p>
+                {/* <p className={styles.timer_start}>0:00</p> */}
+                <div className={styles.slider}><Slider value={props.currentTime} minvalue={0} maxvalue={props.duration} handleChange={props.handleTrackClick}/></div>
+                {/* <p className={styles.timer_end}>3:55</p> */}
+                <p className={styles.timer_end}>{convertTime(props.duration)}</p>
+            </div>
+    )
+}
