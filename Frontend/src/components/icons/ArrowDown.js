5999029356a615eb3b7b9d2b030cfc71ed3af380
Pedro Grigorio <pedrooh.hxh@gmail.com>
2023-04-15 14:42:26 -0300

    navbar and sidemenu restructured

---
+++ Frontend/src/components/icons/ArrowDown.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ArrowDown = ({size}) => {
+    
+    const fill = 'white';
+    return(
+        <svg role="img" height={size} width={size} aria-hidden="true" class="Svg-sc-ytk21e-0 gQUQL eAXFT6yvz37fvS1lmt6k" viewBox="0 0 16 16" data-encore-id="icon">
+            <path d="m14 6-6 6-6-6h12z" fill={fill}/>
+        </svg>
+    )
+}
+
+export default ArrowDown
--- Frontend/src/components/layout/NavBar/NavBar.js
+++ Frontend/src/components/layout/NavBar/NavBar.js
@@ -1,8 +1,10 @@
-import ForwardNav from '../../icons/ForwardNav'
-import BackwardNav from '../../icons/BackwardNav'
-import SearchBox from '../../ui/SearchBox/SearchBox';
-import styles from './NavBar.module.css' 
 import { useEffect, useState } from 'react';
+import styles from './NavBar.module.css' 
+import Forward from '../../icons/Forward'
+import Backward from '../../icons/Backward';
+import SearchBox from '../../ui/SearchBox/SearchBox';
+import ArrowDown from '../../icons/ArrowDown';
+import User from '../../icons/User';
 
 export default function NavBar(props) {
 
@@ -14,16 +16,24 @@ export default function NavBar(props) {
 
     return ( 
         <nav className={styles.navbar}>
-            <div className={styles.nav_button}>
-                <div id={styles.backward}><BackwardNav size={32} /></div>
-                {width > 1024 && <div id={styles.forward}><ForwardNav size={32} /></div>}
-            </div>
-            <div className={styles.page_header}>
-                <SearchBox/>
-            </div>
-            <div className={styles.profile}>
-                {/* <Profile /> */}
-            </div>
+            <header>
+                <div className={styles.nav_control}>
+                    <button><Backward size='16'/></button>
+                    <button><Forward size='16'/></button>
+                </div>
+                <div className={styles.page_top_bar_content}>
+                    <div>
+                        <SearchBox/>
+                    </div>
+                </div>
+                <button className={styles.user}>
+                    <div className={styles.figure_container}>
+                        <div><User size='16'/></div>
+                    </div>
+                    <span>Pedro Grigorio</span>
+                    <ArrowDown size='16'/>
+                </button>
+            </header>
         </nav>
     )
 }
